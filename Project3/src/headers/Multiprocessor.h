55262f5e86133b73fd7718d994d7fb84ee7dda95
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-01 13:36:30 -0400

    Initial attempt at creating the correct number of processes for a given commandFile.

---
+++ Project3/src/headers/Multiprocessor.h
@@ -0,0 +1,18 @@
+#ifndef MULTIPROCESSOR
+#define MULTIPROCESSOR
+
+/* Compiler/OS Headers */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+/* User Created Headers */
+#include "constants.h"
+#include "DoublyLinkedList.h"
+
+/* Function Prototypes */
+void create_map_processes(DoublyLinkedList *);
+
+#endif
---
+++ Project3/src/includes/Multiprocessor.c
@@ -0,0 +1,30 @@
+/*
+** File:         Multiprocessor.c
+** Author:       Jacob Taylor Cassady
+** Description:
+*/
+
+/* User Created Headers */
+#include "../headers/Multiprocessor.h"
+
+/* Function Defintions */
+void create_map_processes(DoublyLinkedList *directoryPaths) {
+    pid_t pid_array[directoryPaths->size] = 0;
+
+    for(unsigned int i = 0; i < directoryPaths->size; i++) {
+      pid_array[i] = fork(); /* fork a child process */
+
+      if (pid_array[i] < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed\n");
+        exit(-1);
+      } else if (pid_array[i] == 0) { /* child process */
+        printf("I am the child with pid = %d, n = %d\n", getpid(), pid_array[i]);
+        return execlp("/bin/ls", "ls", NULL);
+      } else { /* parent process */
+        printf("I am the parent with pid = %d, n = %d\n", getpid(), pid_array[i]);
+        wait(NULL); /* parent will wait for the child to complete */
+        printf("Child Complete\n");
+        exit(0);
+      }
+    }
+}
