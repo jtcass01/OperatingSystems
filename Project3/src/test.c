724af45c04fb5a29f32f7ab7d18c89b965af1b34
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-04 16:00:41 -0500

    Attempting to retrieve file_list from target directory.

--- Project3/src/test.c
+++ Project3/src/test.c
@@ -1,2 +1,25 @@
+#include "headers/FileParser.h"
+#include "headers/DoublyLinkedList.h"
 
+int main(int argc, char *argv[]) {
+	char *directoryPath;
 
+	if (argc != 2) {
+		printf("Invalid number of command line arguments.\n");
+
+		return 1;
+	}
+	else {
+		directoryPath = *(argv + 1);
+	}
+
+	DoublyLinkedList *file_list = dll_create();
+
+	retrieve_file_list(file_list, directoryPath);
+
+	dll_print(file_list);
+
+	dll_destroy(file_list);
+
+	return 0;
+}
