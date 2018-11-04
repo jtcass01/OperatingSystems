b53306c741be87b0a151c381d6492cfd5832d5eb
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-04 14:52:59 -0500

    Working on creating n processes.

--- Project3/src/includes/Multiprocessor.c
+++ Project3/src/includes/Multiprocessor.c
@@ -9,37 +9,34 @@
 
 /* Function Defintions */
 void create_map_processes(DoublyLinkedList *directoryPaths) {
-	dll_print(directoryPaths);
 	Node *currentProcess = directoryPaths->head;
-	pid_t n;
-	pid_t *indicators = initialize_process_indicator_array(directoryPaths->size);
+	pid_t *process_indicators[directoryPaths->size];
 
-    while(currentProcess != NULL) {
-      n = fork(); /* fork a child process */
+	// DEBUG -- Display directory paths passed
+	dll_print(directoryPaths);
 
-      if (n < 0) { /* error occurred */
-        fprintf(stderr, "Fork Failed\n");
-        exit(-1);
-      } else if (n == 0) { /* child process */
-        printf("I am the child with pid = %d, n = %d\n", getpid(), n);
-        return execlp("/bin/ls", "ls", NULL);
-      } else { /* parent process */
-        printf("I am the parent with pid = %d, n = %d\n", getpid(), n);
-        wait(NULL); /* parent will wait for the child to complete */
-        printf("Child Complete\n");
-        exit(0);
-      }
-
-      currentProcess = currentProcess->nextNode;
-    }
-}
-
-pid_t *initialize_process_indicator_array(int number_of_processes) {
-	pid_t *process_indicators[number_of_processes];
-
-	for (int process_index = 0; process_index < number_of_processes; process_index++) {
+	// Initialize the process indicators to 0.
+	for (int process_index = 0; process_index < directoryPaths->size; process_index++) {
 		process_indicators[process_index] = 0;
+		process_indicators[process_index] = fork(); /* fork a child process */
+
+		if (process_indicators[process_index] < 0) { /* error occurred */
+			fprintf(stderr, "Fork Failed\n");
+			exit(-1);
+		}
+		else if (process_indicators[process_index] == 0) { /* child process */
+			printf("I am the child with pid = %d, n = %d\n", getpid(), process_indicators[process_index]);
+			execlp("/bin/ls", "ls", NULL);
+		}
+		else { /* parent process */
+			printf("I am the parent with pid = %d, n = %d\n", getpid(), process_indicators[process_index]);
+			wait(NULL); /* parent will wait for the child to complete */
+			printf("Child Complete\n");
+			exit(0);
+		}
+
+		currentProcess = currentProcess->nextNode;
+
 	}
 
-	return process_indicators;
 }
