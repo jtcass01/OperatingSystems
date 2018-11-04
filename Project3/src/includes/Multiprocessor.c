6fea21fb8b4639e471baf62e1b53e5286ff95aa7
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-04 14:17:46 -0500

    Working on creating n processes.

--- Project3/src/includes/Multiprocessor.c
+++ Project3/src/includes/Multiprocessor.c
@@ -10,18 +10,19 @@
 /* Function Defintions */
 void create_map_processes(DoublyLinkedList *directoryPaths) {
     Node *currentProcess = directoryPaths->head;
+	pid_t n;
 
     while(currentProcess != NULL) {
-      currentProcess->id = fork(); /* fork a child process */
+      n = fork(); /* fork a child process */
 
-      if (currentProcess->id < 0) { /* error occurred */
+      if (n < 0) { /* error occurred */
         fprintf(stderr, "Fork Failed\n");
         exit(-1);
-      } else if (currentProcess->id == 0) { /* child process */
-        printf("I am the child with pid = %d, n = %d\n", getpid(), currentProcess->id);
+      } else if (n == 0) { /* child process */
+        printf("I am the child with pid = %d, n = %d\n", getpid(), n);
         return execlp("/bin/ls", "ls", NULL);
       } else { /* parent process */
-        printf("I am the parent with pid = %d, n = %d\n", getpid(), currentProcess->id);
+        printf("I am the parent with pid = %d, n = %d\n", getpid(), n);
         wait(NULL); /* parent will wait for the child to complete */
         printf("Child Complete\n");
         exit(0);
