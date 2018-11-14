feb32cc1931053a2d03ba8cfbbf8f457054627a5
Jacob Taylor Cassady <jtcass01@louisville.edu>
2018-11-14 15:32:14 -0500

    Error occuring at the end of sender.  Otherwise the issue with the threading seems to be fixed.

--- Project3/src/includes/Multiprocessor.c
+++ Project3/src/includes/Multiprocessor.c
@@ -63,11 +63,11 @@ void create_map_threads(char *directory_path, int bufferSize) {
 	sem_destroy(&empty);
 	sem_destroy(&full);
 
-	printf("Ending Microprocessor.\n");
+	printf("Ending create_map_threads.\n");
 }
 
 void create_map_processes(DoublyLinkedList *directoryPaths, int bufferSize) {
-#if DEBUG
+#if 1
 	printf("I am the parent process (pid: %d).  Beginning to create %d processes to serve each directory path.\n\n", getpid(), directoryPaths->size);
 	dll_print(directoryPaths);
 #endif
@@ -75,22 +75,21 @@ void create_map_processes(DoublyLinkedList *directoryPaths, int bufferSize) {
 	// Initialize the parent process indicators to 0.  Start processes
 	for (int process_index = 0; process_index < directoryPaths->size; process_index++) {
 		if (fork() == 0) {
-#if DEBUG
+#if 1
 			printf("I am the child with pid = %d and process_index = %d, from parent = %d, BEGINNING WORK.\n", getpid(), process_index, getppid());
 #endif
 
 			// Do work with processes
 			Node *process_node = dll_find_node_by_index(directoryPaths, process_index);
-#if DEBUG
+#if 1
 			printf("Creating map threads for directory: %s\n", process_node->word);
 #endif
 			create_map_threads(process_node->word, bufferSize);
 
 			// exit child process
-#if DEBUG
+#if 1
 			printf("I am the child with pid = %d and process_index = %d, from parent = %d, DONE.\n", getpid(), process_index, getppid());
 #endif
-
 			exit(0);
 		}
 	}
